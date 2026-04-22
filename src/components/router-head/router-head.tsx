ecfb4a135d9418dfe83cd61bfbf9ee8dcff18df0
Seba Cardoso <sebastiancardoso92@gmail.com>
2026-04-22 18:16:50 -0300

    Agrego favicon

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -16,7 +16,6 @@ export const RouterHead = component$(() => {
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="icon" type="image/png" href="/favicon.png" />
       <link rel="apple-touch-icon" type="image/png" href="/favicon.png" />
-      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       <link
